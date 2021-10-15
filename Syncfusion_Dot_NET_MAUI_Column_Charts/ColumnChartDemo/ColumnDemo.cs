83cd48e28d3c1e29eb67efa78390aa857a6ee2d6
saravananmadhesh <saravananmadhesh20@gmail.com>
2021-10-15 10:54:14 +0530

    Update Demo sample

---
+++ Syncfusion_Dot_NET_MAUI_Column_Charts/ColumnChartDemo/ColumnDemo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using Syncfusion.Maui.Charts;
+
+namespace Charts
+{
+    public class ColumnDemo : ContentPage
+    {
+        public ColumnDemo()
+        {
+			SfCartesianChart chart = new SfCartesianChart();
+			chart.BindingContext = new ViewModel.ViewModel();
+
+			//Initializing Primary Axis
+			CategoryAxis primaryAxis = new CategoryAxis();
+			chart.PrimaryAxis = primaryAxis;
+
+			//Initializing Secondary Axis
+			NumericalAxis secondaryAxis = new NumericalAxis();
+			chart.SecondaryAxis = secondaryAxis;
+
+			//Initialize series
+			var binding = new Binding() { Path = "Data" };
+			var columnSeries = new ColumnSeries()
+			{
+				XBindingPath = "Country",
+				YBindingPath = "Counts",
+				ShowDataLabels = true,
+				Background = new SolidColorBrush(Color.FromArgb("#314A6E")),
+			};
+
+			columnSeries.SetBinding(ChartSeries.ItemsSourceProperty, binding);
+			chart.Series.Add(columnSeries);
+
+			//Chart title
+			var title = new Label()
+			{
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				HorizontalTextAlignment = Microsoft.Maui.TextAlignment.Center,
+				Text = "Internet Users",
+				Margin = new Microsoft.Maui.Thickness(5, 10, 5, 10),
+			};
+			chart.Title = title;
+
+			var grid = new Grid() 
+			{ 
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				Padding = new Microsoft.Maui.Thickness(20),
+			};
+
+			grid.Children.Add(chart);
+
+			this.Content = grid;
+		}
+	}
+}
---
+++ Syncfusion_Dot_NET_MAUI_Column_Charts/ColumnChartDemo/ViewModel/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charts.ViewModel
+{
+    public class ViewModel
+    {
+        public ObservableCollection<Model> Data { get; set; }
+
+        public ViewModel()
+        {
+            Data = new ObservableCollection<Model>()
+            {
+                new Model("Korea",39),
+                new Model("India",20),
+                new Model("Africa",  61),
+                new Model("China",65),
+                new Model("France",45),
+            };
+        }
+    }
+
+
+    public class Model
+    {
+        public string Country { get; set; }
+
+        public double Counts { get; set; }
+
+        public Model(string name , double count)
+        {
+            Country = name;
+            Counts = count;
+        }
+    }
+
+}
