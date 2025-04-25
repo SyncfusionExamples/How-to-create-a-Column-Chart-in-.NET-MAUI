e8b30f3e83d933ab52e2635126334d8610c4a0d2
Harsha <harshamidadhala.k@syncfusion.com>
2025-04-25 18:11:39 +0530

    updated sample with .net 9 version

--- Syncfusion_Dot_NET_MAUI_Column_Charts/ColumnChartDemo/Platforms/iOS/AppDelegate.cs
+++ Syncfusion_Dot_NET_MAUI_Column_Charts/ColumnChartDemo/Platforms/iOS/AppDelegate.cs
@@ -1,9 +1,10 @@
 ﻿using Foundation;
 
-namespace ColumnChartDemo;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
+namespace ColumnChartDemo
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
 }
